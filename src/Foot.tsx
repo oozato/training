2a7faa0c7180aa1633abde5a0280ec972fbc62bf
Asamatsu <e175771@ie.u-ryukyu.ac.jp>
2020-06-11 11:06:50 +0900

    Icon調整、body削除

--- src/Foot.tsx
+++ src/Foot.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./style.css";
-import polygon from "./polygon.svg";
-import twitter from "./twitter.svg";
-import facebook from "./facebook.svg";
-import jobantennaIcon from "./jobantennaIcon.svg";
-import searchButton from "./searchButton.svg";
+import ReturnIcon from "./Icon/ReturnIcon.svg";
+import twitter from "./Icon/twitter.svg";
+import facebook from "./Icon/facebook.svg";
+import BlackJobantennaIcon from "./Icon/BlackJobantennaIcon.svg";
+import searchButton from "./Icon/searchButton.svg";
 
 function Foot() {
   return (
     <div className="wrap">
       <div className="footer">
         <a href="#" className="footer-page_return">
-          <img src={polygon} />
+          <img src={ReturnIcon} />
           <div className="footer-page_return-text">ページの先頭へ戻る</div>
         </a>
         <div className="footer-bar"></div>
@@ -60,7 +60,10 @@ function Foot() {
         <div className="footer-foot">
           <div className="footer-foot-brand">
             <a href="#">
-              <img src={jobantennaIcon} className="footer-foot-brand-icon" />
+              <img
+                src={BlackJobantennaIcon}
+                className="footer-foot-brand-icon"
+              />
             </a>
 
             <div className="footer-foot-brand-copyright">
--- src/Head.tsx
+++ src/Head.tsx
@@ -1,45 +1,43 @@
 import React from "react";
-import jobantennaIcon2 from "./jobantennaIcon2.svg";
+import WhiteJobantennaIcon from "./Icon/WhiteJobantennaIcon.svg";
 import "./style.css";
 
 function Head() {
   return (
-    <body>
-      <div className="wrap">
-        <header className="head-fixed">
-          <div className="head-bk">
-            <div className="head">
-              <div className="head-left">
-                <img src={jobantennaIcon2} className="head-left-icon" />
-                <a href="#" className="head-left-search">
-                  求人を探す
-                </a>
-                <a href="#" className="head-left-search">
-                  会社を探す
-                </a>
-                <a href="#" className="head-left-search">
-                  転職相談をする
-                </a>
-                <a href="#" className="head-left-trouble">
-                  採用にお困りの企業様へ
-                </a>
-              </div>
-              <div className="head-right">
-                <a href="#" className="head-right-signup head-right-icon">
-                  会員登録
-                </a>
-                <a href="#" className="head-right-login head-right-icon">
-                  ログイン
-                </a>
-                <a href="#" className="head-right-menu head-right-icon">
-                  メニュー
-                </a>
-              </div>
+    <div className="wrap">
+      <header className="head-fixed">
+        <div className="head-bk">
+          <div className="head">
+            <div className="head-left">
+              <img src={WhiteJobantennaIcon} className="head-left-icon" />
+              <a href="#" className="head-left-search">
+                求人を探す
+              </a>
+              <a href="#" className="head-left-search">
+                会社を探す
+              </a>
+              <a href="#" className="head-left-search">
+                転職相談をする
+              </a>
+              <a href="#" className="head-left-trouble">
+                採用にお困りの企業様へ
+              </a>
+            </div>
+            <div className="head-right">
+              <a href="#" className="head-right-signup head-right-icon">
+                会員登録
+              </a>
+              <a href="#" className="head-right-login head-right-icon">
+                ログイン
+              </a>
+              <a href="#" className="head-right-menu head-right-icon">
+                メニュー
+              </a>
             </div>
           </div>
-        </header>
-      </div>
-    </body>
+        </div>
+      </header>
+    </div>
   );
 }
 
